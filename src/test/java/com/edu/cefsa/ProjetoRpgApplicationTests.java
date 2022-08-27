01bd9bb7271a26e228013d4d76caeda1a28a5d7a
Victor Cardoso <59899335+V6Cardoso@users.noreply.github.com>
2022-08-27 20:30:56 -0300

    Initial commit

---
+++ src/test/java/com/edu/cefsa/ProjetoRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.cefsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
