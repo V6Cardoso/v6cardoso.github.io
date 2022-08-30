e4fa4bc8bec523a5daa377c2db5fb1c9de1379e6
Victor Cardoso <59899335+V6Cardoso@users.noreply.github.com>
2022-08-30 00:01:01 -0300

    cleaned branch

--- src/main/webapp/resources/static/index/index.js
+++
@@ -1,7 +0,0 @@
-function handleClick(clickedId)
-  {
-     if(clickedId == "customerId")
-       document.getElementById('valor').value = "customer";
-     else
-       document.getElementById('valor').value = "company";
-  }
