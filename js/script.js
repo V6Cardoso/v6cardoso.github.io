0002e8d7afcec24691ef753b143a5e7050e24d1c
Victor Cardoso <59899335+V6Cardoso@users.noreply.github.com>
2022-09-05 01:12:17 -0300

    dark mode working

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 let dark = false;
 
-darkMode();
+window.onload = function() {
+    value = localStorage.getItem("darkMode",this.dark);
+    this.dark = (value === 'true');
+    if (document.getElementById('darkmode') != null) {
+        document.getElementById('darkmode').checked = this.dark;
+    }
+    darkMode();
+ }
+
 
 function darkMode() {
     const collection = document.getElementsByClassName('box');
-    if (document.getElementById('darkmode') != null)
+    if (document.getElementById('darkmode') != null) {
         checked = document.getElementById('darkmode').checked;
-    else
+    }
+    else {
         checked = this.dark;
+    }
         
     if (checked) {
         this.dark = true;
@@ -21,7 +31,18 @@ function darkMode() {
             collection[i].style.backgroundColor = 'lightyellow';
         }
     }
-}
 
+    if (document.getElementById('fellowshipImage') != null) {
+        if (this.dark) {
+            document.getElementById('fellowshipImage').src = 'img/fellowship-gray.png';
+            document.getElementById('title').style.color = '#bfbfbf';
+        }
+        else {
+            document.getElementById('fellowshipImage').src = 'img/fellowship.png';
+            document.getElementById('title').style.color = 'black';
+        }
+    }
+    localStorage.setItem('darkMode', this.dark);
+}
 
 
