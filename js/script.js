ad6d6c6178340e222778dff3ba240a9f4039a366
Victor Cardoso <59899335+V6Cardoso@users.noreply.github.com>
2022-09-11 03:17:39 -0300

    updated origin

--- js/script.js
+++ js/script.js
@@ -3,46 +3,61 @@ let dark = false;
 window.onload = function() {
     value = localStorage.getItem("darkMode",this.dark);
     this.dark = (value === 'true');
-    if (document.getElementById('darkmode') != null) {
+
+    if (document.getElementById('darkmode') != null)
         document.getElementById('darkmode').checked = this.dark;
-    }
+
     darkMode();
  }
 
 
 function darkMode() {
-    const collection = document.getElementsByClassName('box');
+    
+
     if (document.getElementById('darkmode') != null) {
-        checked = document.getElementById('darkmode').checked;
-    }
-    else {
-        checked = this.dark;
-    }
-        
-    if (checked) {
-        this.dark = true;
-        for (let i = 0; i < collection.length; i++) {
-            collection[i].style.backgroundColor = '#444';
-        }
-    }
-    else {
-        this.dark = false;
-        for (let i = 0; i < collection.length; i++) {
-            collection[i].style.backgroundColor = 'lightyellow';
-        }
+        this.dark = document.getElementById('darkmode').checked;
+        localStorage.setItem('darkMode', this.dark);
     }
 
-    if (document.getElementById('fellowshipImage') != null) {
-        if (this.dark) {
-            document.getElementById('fellowshipImage').src = 'img/fellowship-gray.png';
-            document.getElementById('title').style.color = '#bfbfbf';
+    const lightColor = '#d3d3d3';
+    const textDarkColor = 'black'
+    const grayishColor = 'gray';
+
+    const box = document.getElementsByClassName('box');
+    const fellowshipImage = document.getElementById('fellowshipImage');
+    const text = document.getElementsByClassName('text-name');
+    const image = document.getElementsByClassName('image');
+        
+
+    if (this.dark) {
+        for (let i = 0; i < box.length; i++)
+            box[i].style.backgroundColor = '#444';
+
+        for (let i = 0; i < text.length; i++)
+            text[i].style.color = lightColor;
+        
+        for (let i = 0; i < image.length; i++)
+            image[i].style['border-color'] = lightColor;
+        
+        if (fellowshipImage != null) {
+            fellowshipImage.src = 'img/fellowship-gray.png';
         }
-        else {
-            document.getElementById('fellowshipImage').src = 'img/fellowship.png';
-            document.getElementById('title').style.color = 'black';
+        
+    }
+    else {
+        for (let i = 0; i < box.length; i++)
+            box[i].style.backgroundColor = 'lightyellow';
+
+        for (let i = 0; i < text.length; i++)
+            text[i].style.color = textDarkColor;
+        
+        for (let i = 0; i < image.length; i++)
+            image[i].style['border-color'] = grayishColor;
+
+        if (fellowshipImage != null) {
+            fellowshipImage.src = 'img/fellowship.png';
         }
     }
-    localStorage.setItem('darkMode', this.dark);
 }
 
 
